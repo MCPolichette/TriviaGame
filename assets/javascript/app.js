b609f1a5ac81c6b8f81f0b3d72936b90eb1818b7
MCPolichette <mark@polichette.com>
2018-05-13 14:27:36 -0600

    timers Suck

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,20 @@
 
 // questionArray is for shuffling questions
+questionsAsked = 2;
 var questionList = [
 
     // Objects as Trivia Questions
     // Using the term ITEM for starting variable.  because I want to use the term QUESTION for a variable inside the object
-    item1 = {
+    {
         question: "X Will you choose the right answer?",
         answer: "X The right option",
+        wrongAnswer1: "X wrong choice two",
         wrongAnswer2: "X wrong choice two",
         wrongAnswer3: "X wrong choice three",
         questionImg: "",
         answerImg: "",
-
     },
-    item2 = {
+    {
         question: "Choose the right answer",
         answer: "The right option",
         wrongAnswer1: "wrong choice one",
@@ -21,7 +22,24 @@ var questionList = [
         wrongAnswer3: "wrong choice three",
         questionImg: "",
         answerImg: "",
-
+    },
+    {
+        question: "Choose the right answer",
+        answer: "The right option2222222222222222222",
+        wrongAnswer1: "2wrong choice one",
+        wrongAnswer2: "2wrong choice two",
+        wrongAnswer3: "2wrong choice three",
+        questionImg: "",
+        answerImg: "",
+    },
+    {
+        question: "Choose the right answer4444444444444",
+        answer: "The right option",
+        wrongAnswer1: "3wrong choice one",
+        wrongAnswer2: "3wrong choice two",
+        wrongAnswer3: "3wrong choice three",
+        questionImg: "",
+        answerImg: "",
     },
 
 ]
@@ -33,13 +51,14 @@ var timerOn = false;
 var correct = 0;
 var wrong = 0;
 var unanswered = 0;
-
+var questionIndex = 0;
 // Functions:
 function gameReset() {
     // resets game
     wrong = 0;
     unanswered = 0;
     correct = 0;
+    questionIndex = 0;
 }
 function shuffle(array) {
     // shuffles any array
@@ -65,30 +84,54 @@ function countDownTimer() {
             timerOn = false;
             console.log(timerOn);
             stop();
+            nextQuestion();
+            $("#confirm").text("YOU RAN OUT OF TIME");
+            console.log(questionIndex);
+            // questionReset(questionList[questionIndex]);
         }
     }
 }
 function stop() {
     clearInterval(secondID);
 }
+function nextQuestion() {
+    // where x is the current question
+    $(".game").addClass("hidden");
+    $("#scoreCard").removeClass("hidden");
+    console.log(questionIndex);
+    questionIndex++;
+    console.log(questionIndex);
+    console.log(questionList[questionIndex]);
+    setTimeout(function () { questionReset(questionList[questionIndex]) }, 5000);
+}
 
-function questionReset(item) {
-    // resets next questsion
-    $('#q').text(item.question);
-    var answerArray = [item.answer, item.wrongAnswer1, item.wrongAnswer2, item.wrongAnswer3];
-    // answerArray is used for shuffling answers
-    console.log(item.question);
-    shuffle(answerArray);
-    console.log(answerArray);
 
-    var displayArray = ["#a1", "#a2", "#a3", "#a4"];
-    for (i = 0; i < displayArray.length; i++) {
-        $(displayArray[i]).text(answerArray[i]);
+function questionReset(object) {
+    if (questionIndex > questionList.length) {
+        endGame();
+        console.log("GAMEOVER");
+    } else {
+        $(".game").removeClass("hidden");
+        $("#scoreCard").addClass("hidden");
+        stop();
+        seconds = 10;
+        countDownTimer();
+        // resets next questsion
+        $('#q').text(object.question);
+        var answerArray = [object.answer, object.wrongAnswer1, object.wrongAnswer2, object.wrongAnswer3];
+        // answerArray is used for shuffling answers
+        console.log(object.question);
+        shuffle(answerArray);
+        console.log(answerArray);
 
-    };
-    seconds = 10;
-    // countDownTimer();
-    console.log("test")
+        var displayArray = ["#a1", "#a2", "#a3", "#a4"];
+        for (i = 0; i < displayArray.length; i++) {
+            $(displayArray[i]).text(answerArray[i]);
+
+        };
+
+        // countDownTimer();
+    }
 }
 // console.log("does this work?" + displayArray[i]);
 function congratsDude(object) {
@@ -106,33 +149,38 @@ function getYourShitTogether(object) {
     wrong++
 
 }
+
+function endGame() {
+    console("GAMEOVER");
+}
 // GAME BEGINS HERE.
 window.onload =
 
     $("#start").on('click', function () {
         console.log("gameBegins");
+
         // set game up
         gameReset();
-        currentQuestion = questionList[0];
+
         $(".game").removeClass("hidden");
         $("#start").addClass("hidden");
-
-        questionReset(currentQuestion)
+        countDownTimer()
+        questionReset(questionList[questionIndex])
         $(".possibleAnswer").on('click', function () {
             var click = $(this);
             var check = click.text();
 
-            if (check === currentQuestion.answer) {
-                congratsDude(currentQuestion);
+            if (check === questionList[questionIndex].answer) {
+                congratsDude(questionList[questionIndex]);
                 console.log("correct" + correct);
-                stop();
-                questionReset(currentQuestion);
+                setTimeout(nextQuestion(questionList[questionIndex]), 5000);
+
 
             } else {
                 getYourShitTogether(questionList[1]);
                 console.log("wrong" + wrong);
-                stop()
-                questionReset(questionList[1]);
+                setTimeout(nextQuestion(questionList[questionIndex]), 5000);
+
             }
 
 
